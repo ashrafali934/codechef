0d1bded1b9ba3002183ba8cee82924220f394ad6
shahid <shahidshah77562@gmail.com>
2024-12-06 13:51:02 +0530

    Add files via upload

--- Paper Cutting.java
+++ Paper Cutting.java
@@ -6,16 +6,15 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc=new Scanner(System.in);
-		int t=sc.nextInt();
-		for(int i=0;i<t;i++)
-		{
-		    int a=sc.nextInt();
-		    int b=sc.nextInt();
-		    int c=(a/b)*(a/b);
-		    System.out.println(c);
-		    
-		}
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	for(int i=0;i<t;i++)
+	{
+	    int a=sc.nextInt();
+	    int b=sc.nextInt();
+	    int c=(a/b)*(a/b);
+	    System.out.println(c);
+	}
 
 	}
 }
